2d4b9921a58addd8d2a8d3087544f6b83ed97f72
su <su@sus-MacBook-Air.home>
2024-10-10 21:14:22 -0700

    fixing issues on stage 1

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # main.py
 
 import sys
-from PyQt5.QtWidgets import QApplication
+import os
+
+# Add the directory containing 'gui' to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, current_dir)
+
 from gui.main_window import MainWindow
+from PyQt5.QtWidgets import QApplication
 
 def main():
     app = QApplication(sys.argv)
-
     main_window = MainWindow()
     main_window.show()
-
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
